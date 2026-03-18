b31e326c3354f5f060bd6e0436beca80d61b7391
khalloufiajroureda <khalloufiajroureda001.alu@stemgranada.com>
2026-03-18 12:42:06 +0100

    pepe

--- testing/src/main/java/es/stemgranada/ed/testing/calculadora/Calculadora.java
+++ testing/src/main/java/es/stemgranada/ed/testing/calculadora/Calculadora.java
@@ -1,5 +1,4 @@
-
-package es.stemgranada.practica_test_unitario;
+package es.stemgranada.ed.testing.calculadora;
 
 /**
  * Clase que proporciona operaciones matemáticas básicas y utilidades
@@ -9,6 +8,10 @@ public class Calculadora {
 
     /**
      * Suma dos números enteros.
+     *
+     * @param a primer sumando
+     * @param b segundo sumando
+     * @return la suma de a y b
      */
     public int sumar(int a, int b) {
         return a + b;
@@ -16,6 +19,10 @@ public class Calculadora {
 
     /**
      * Resta dos números enteros.
+     *
+     * @param a minuendo
+     * @param b sustraendo
+     * @return la resta de a y b
      */
     public int restar(int a, int b) {
         return a - b;
@@ -23,6 +30,10 @@ public class Calculadora {
 
     /**
      * Multiplica dos números enteros.
+     *
+     * @param a primer factor
+     * @param b segundo factor
+     * @return el producto de a y b
      */
     public int multiplicar(int a, int b) {
         return a * b;
@@ -31,6 +42,9 @@ public class Calculadora {
     /**
      * Divide dos números.
      *
+     * @param a dividendo
+     * @param b divisor
+     * @return el cociente de dividir a entre b
      * @throws IllegalArgumentException si el divisor es cero
      */
     public double dividir(double a, double b) {
@@ -43,6 +57,9 @@ public class Calculadora {
     /**
      * Calcula la potencia base elevado a exponente.
      *
+     * @param base valor base de la potencia
+     * @param exponente número de veces que se multiplica la base por sí misma
+     * @return el resultado de elevar la base al exponente indicado
      * @throws IllegalArgumentException si el exponente es negativo
      */
     public double potencia(double base, int exponente) {
@@ -62,6 +79,8 @@ public class Calculadora {
     /**
      * Calcula la media aritmética de un conjunto de valores.
      *
+     * @param valores conjunto de valores del que se calculará la media
+     * @return la media aritmética de los valores proporcionados
      * @throws IllegalArgumentException si el array es null o está vacío
      */
     public double media(double[] valores) {
@@ -80,6 +99,9 @@ public class Calculadora {
 
     /**
      * Determina si un número es par.
+     *
+     * @param numero número que se quiere comprobar
+     * @return true si el número es par; false en caso contrario
      */
     public boolean esPar(int numero) {
         return numero % 2 == 0;
@@ -88,6 +110,8 @@ public class Calculadora {
     /**
      * Calcula el factorial de un número.
      *
+     * @param numero número del que se calculará el factorial
+     * @return el factorial del número indicado
      * @throws IllegalArgumentException si el número es negativo
      */
     public long factorial(int numero) {
@@ -108,4 +132,3 @@ public class Calculadora {
         return resultado;
     }
 }
-
